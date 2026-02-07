bdb3d9acd8ec5754f35ca9cdd6eb922e3291223a
Brittani Lovelace <blovelace84@gmail.com>
2026-02-06 19:32:58 -0500

    Trying to add metadata to see what it looks like

--- chao-garden/app/layout.tsx
+++ chao-garden/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Chao Garden Layout",
+  description:
+    "This is a layout for the Chao Garden page, providing a consistent structure and styling across the application.",
+};
 
 export default function RootLayout({
   children,
