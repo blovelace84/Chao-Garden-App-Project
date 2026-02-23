cbb22f477cfd661a6be410e9354b7647feae583c
Brittani Lovelace <blovelace84@gmail.com>
2026-02-22 20:05:47 -0500

    Delete

--- chao-garden/app/components/ui/Card.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Card({ children }: { children: React.ReactNode }) {
-  return <div className="bg-white rounded-2xl shadow-lg p-6">{children}</div>;
-}
